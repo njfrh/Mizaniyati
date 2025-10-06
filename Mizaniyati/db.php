b21c2f27dba234b32a4976e063f71c40de355ca3
wadha <wad7202a@gmail.com>
2025-10-06 08:32:38 +0300

    الشي اللي عدلته

--- Mizaniyati/db.php
+++ Mizaniyati/db.php
@@ -1,12 +1,17 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mizaniyati"; // اسم قاعدة بياناتك
+$host = "127.0.0.1";
+$port = 3306;
+$db   = "mizaniyati";
+$user = "root";
+$pass = "root";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
+// التأكد من الاتصال
 if ($conn->connect_error) {
-    die("فشل الاتصال: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
+
+// ضبط الترميز لدعم العربية
+$conn->set_charset("utf8mb4");
 ?>
